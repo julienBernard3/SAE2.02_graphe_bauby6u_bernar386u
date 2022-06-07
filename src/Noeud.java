1085427cfeacaa4d43c02e6d86a33676ef23ec41
Julien Bernard <julienbernardjb88@gmail.com>
2022-06-07 08:39:12 +0200

    ajout noeud

---
+++ src/Noeud.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Noeud {
+    /**
+     * Nom du nœud
+     */
+    private String nom;
+    /**
+     * Liste des arcs reliant ce nœud `a ses nœuds adjacents
+     */
+    private List<Arc> adj;
+
+    /**
+     * Constructeur de noeud initialisant le nom du noeud et adj a une liste vide
+     * @param nom nom du noeud
+     */
+    public Noeud (String nom){
+        this.nom = nom;
+        this.adj = new List<Arc>();
+    }
+
+}
