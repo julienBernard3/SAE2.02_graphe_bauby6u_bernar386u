5db9b682fe6a20b494fd203df794d8e354400e0c
Gaspard <g.bauby1@gmail.com>
2022-06-07 08:52:28 +0200

    add interface Graphe

---
+++ src/Graphe.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Graphe {
+    /**
+     * Methode qui retourne tous les noms des noeuds du graphe
+     *
+     * @return list des nom
+     */
+    public List<String> listeNoeuds();
+
+    /**
+     * Methode qui retourne la liste des arcs apres le noeuds en param
+     *
+     * @param n le noeud apres lequel on veut recuperer la liste
+     * @return la liste des arc apres le noeud
+     */
+    public List<Arc> suivants(String n);
+
+}
