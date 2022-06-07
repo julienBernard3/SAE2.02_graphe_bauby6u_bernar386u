e8df4321e49317ca66f8f9b6ca3b8bdf6e040d96
Gaspard <g.bauby1@gmail.com>
2022-06-07 08:39:29 +0200

    add Arc.java

---
+++ src/Arc.java
@@ -0,0 +1,15 @@
+public class Arc {
+
+    private String dest;
+    private double cout;
+
+    /**
+     * Constructeur d'un arc
+     * @param dest nom du noeud de destination
+     * @param cout cout ou poids de l arc
+     */
+    public Arc(String dest, double cout) {
+        this.dest = dest;
+        this.cout = (cout < 0) ? 1 : cout;
+    }
+}
