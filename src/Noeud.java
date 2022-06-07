0fbdb6024672630a9efc895c717114eca567ed5e
Gaspard <g.bauby1@gmail.com>
2022-06-07 08:48:02 +0200

    update ajouterArc

--- src/Noeud.java
+++ src/Noeud.java
@@ -13,9 +13,10 @@ public class Noeud {
 
     /**
      * Constructeur de noeud initialisant le nom du noeud et adj a une liste vide
+     *
      * @param nom nom du noeud
      */
-    public Noeud (String nom){
+    public Noeud(String nom) {
         this.nom = nom;
         this.adj = new ArrayList<Arc>();
     }
@@ -23,13 +24,15 @@ public class Noeud {
 
     /**
      * Methode qui ajoute un arc
+     *
      * @param destination destination de l arc
-     * @param cout cout de la destination
+     * @param cout        cout de la destination
      */
-    public void ajouterArc(Noeud destination, int cout){
-
+    public void ajouterArc(Noeud destination, int cout) {
+        this.adj.add(new Arc(destination.nom, cout));
     }
-
-
-
 }
+
+
+
+
