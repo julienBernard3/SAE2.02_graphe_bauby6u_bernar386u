8947ad8a6b63fe12b4046850e9121c69ff256ae6
Julien Bernard <julienbernardjb88@gmail.com>
2022-06-13 15:41:00 +0200

    correctif algo bellman

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -12,7 +12,6 @@ public class BellmanFord {
      */
     public Valeur resoudre(Graphe g, String depart) {
         Valeur v = new Valeur();
-        v.setValeur(depart, 0);
         List<String> listeNoeuds = g.listeNoeuds();
         //parcours listeNoeuds
         for (int i = 0; i < listeNoeuds.size(); i++) {
@@ -21,34 +20,16 @@ public class BellmanFord {
         }
         v.setValeur(depart, 0);
         Valeur ancien = new Valeur();
-        System.out.println("PREMIER"  + v.getValeur(depart));
-        System.out.println(v.getValeur(listeNoeuds.get(0)) + " " + v.getValeur(depart) + g.suivants(listeNoeuds.get(0)).get(0).getPoids());
-        System.out.println("NOM " + listeNoeuds.get(0));
-        System.out.println("NOM " + listeNoeuds.get(1));
-        System.out.println("NOM " + listeNoeuds.get(2));
 
         while (!ancien.equals(v)) {
             ancien = new Valeur(v);
-            System.out.println("VERIFICATION C LA MM VALEUR" + ancien.equals(v));
-
-//            System.out.println(listeNoeuds.size());
-//            System.out.println(g.listeNoeuds().size());
             for (int i = 0; i < listeNoeuds.size(); i++) {
                 for (int j = 0; j < g.suivants(listeNoeuds.get(i)).size(); j++) {
-                    System.out.println(i + " " + j);
-//                    System.out.println("PREMIER"  + v.getValeur(depart));
-                    System.out.println(v.getValeur(listeNoeuds.get(j)) + " " + v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids());
-                    if (v.getValeur(listeNoeuds.get(j)) > (v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids())) {
-                        v.setValeur(listeNoeuds.get(j), v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids());
-                        v.setParent(g.listeNoeuds().get(j), g.listeNoeuds().get(i));
-                        System.out.println("hello");
+                    if (v.getValeur(g.suivants(listeNoeuds.get(i)).get(j).getDest()) > (v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids())) {
+                        v.setValeur(g.suivants(listeNoeuds.get(i)).get(j).getDest(), v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids());
+                        v.setParent(g.suivants(listeNoeuds.get(i)).get(j).getDest(), g.listeNoeuds().get(i));
                     }
 
-                }
-                if (i == 3) {
-                    System.out.println(ancien);;
-                    System.out.println(v);
-                    break;
                 }
             }
         }
