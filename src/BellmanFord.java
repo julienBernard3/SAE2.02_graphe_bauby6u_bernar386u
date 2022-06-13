c2a159d61d4d67efb419a99c10263c56106bcece
Julien Bernard <julienbernardjb88@gmail.com>
2022-06-13 16:19:41 +0200

    correctif algo bellman

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -12,22 +12,21 @@ public class BellmanFord {
      */
     public Valeur resoudre(Graphe g, String depart) {
         Valeur v = new Valeur();
-        List<String> listeNoeuds = g.listeNoeuds();
         //parcours listeNoeuds
-        for (int i = 0; i < listeNoeuds.size(); i++) {
-            v.setParent(listeNoeuds.get(i), null);
-            v.setValeur(listeNoeuds.get(i), Double.POSITIVE_INFINITY);
+        for (int i = 0; i < g.listeNoeuds().size(); i++) {
+            v.setParent(g.listeNoeuds().get(i), null);
+            v.setValeur(g.listeNoeuds().get(i), Double.POSITIVE_INFINITY);
         }
         v.setValeur(depart, 0);
         Valeur ancien = new Valeur();
 
         while (!ancien.equals(v)) {
             ancien = new Valeur(v);
-            for (int i = 0; i < listeNoeuds.size(); i++) {
-                for (int j = 0; j < g.suivants(listeNoeuds.get(i)).size(); j++) {
-                    if (v.getValeur(g.suivants(listeNoeuds.get(i)).get(j).getDest()) > (v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids())) {
-                        v.setValeur(g.suivants(listeNoeuds.get(i)).get(j).getDest(), v.getValeur(listeNoeuds.get(i)) + g.suivants(listeNoeuds.get(i)).get(j).getPoids());
-                        v.setParent(g.suivants(listeNoeuds.get(i)).get(j).getDest(), g.listeNoeuds().get(i));
+            for (int i = 0; i < g.listeNoeuds().size(); i++) {
+                for (int j = 0; j < g.suivants(g.listeNoeuds().get(i)).size(); j++) {
+                    if (v.getValeur(g.suivants(g.listeNoeuds().get(i)).get(j).getDest()) > (v.getValeur(g.listeNoeuds().get(i)) + g.suivants(g.listeNoeuds().get(i)).get(j).getPoids())) {
+                        v.setValeur(g.suivants(g.listeNoeuds().get(i)).get(j).getDest(), v.getValeur(g.listeNoeuds().get(i)) + g.suivants(g.listeNoeuds().get(i)).get(j).getPoids());
+                        v.setParent(g.suivants(g.listeNoeuds().get(i)).get(j).getDest(), g.listeNoeuds().get(i));
                     }
 
                 }
